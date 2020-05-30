7ea694386f305c2898b64bcc076515816d12f803
Atanas Minkov <atanas.minkov@icloud.com>
2020-05-30 04:44:12 +0300

    Updates

---
+++ precache-manifest.bcdcf733be229806bf3b4dec8ae4e37f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7a3691746c72d41a72ac1ad0b0a152",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "35182d52c247a8b8e98d",
+    "url": "/monsters-rolodex/static/css/main.37607756.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35182d52c247a8b8e98d",
+    "url": "/monsters-rolodex/static/js/main.adbbaa67.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
